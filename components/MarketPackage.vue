535116b604b07cb149182395de751b86b195624a
pcnadev <boombs5665@gmail.com>
2024-06-19 19:35:13 +0700

    add

---
+++ components/MarketPackage.vue
@@ -0,0 +1,580 @@
+<template>
+  <div class="M-Create-Backgroud">
+    <div class="box-alert">
+      <div>
+        <img
+          class=""
+          style="margin: 10px 20px"
+          src="~/assets/image/genshin.png"
+        />
+      </div>
+      <div>
+        <div>#0000000000</div>
+        <div>หมดอายุ : 29/06/2029</div>
+      </div>
+    </div>
+    <div class="navtabs">
+      <div class="dis-flex-end">
+        <div class="card-into">
+          <div>
+            <div class="poppular">MOST POPULAR</div>
+          </div>
+
+          <div style="margin-top: 5px">
+            <span class="font-card-top">฿29</span><span>/month</span>
+            <div>เฉลี่ย 0.97 ต่อวัน</div>
+          </div>
+
+          <div class="font-card-top" style="font-size: 24px">Intro</div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />สิทธิพิเศษจากการบริการจาก Brand
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />ได้รับส่วนลดจากร้านค้าเพิ่ม สูงสุด 5 - 50 %
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />มีระบบแจ้งเตือน SMS เมื่อ ทำการถอนเงินสำเร็จ
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />ได้รับสัญลักษณ์ว่าเป็นร้านค้า "Brand"
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />เพิ่มรูปภาพสินค้าได้มากกว่าเดิม ถึง 2 เท่า
+          </div>
+          <div style="width: 100%">
+            <div class="button-but">ซื้อเลย</div>
+          </div>
+        </div>
+        <div class="card-other">
+          <div>
+            <span class="font-card-top-other">฿29</span
+            ><span class="font-right-other">/month</span>
+            <div>เฉลี่ย 0.97 ต่อวัน</div>
+          </div>
+
+          <div class="font-right-other" style="font-size: 24px">Base</div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              สิทธิพิเศษจากการบริการจาก
+              <span class="font-right-other">Brand</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />ได้รับส่วนลดจากร้านค้าเพิ่ม สูงสุด 5 - 50 %
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />มีระบบแจ้งเตือน SMS เมื่อ ทำการถอนเงินสำเร็จ
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              ได้รับสัญลักษณ์ว่าเป็นร้านค้า
+              <span class="font-right-other">"Brand"</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />เพิ่มรูปภาพสินค้าได้มากกว่าเดิม ถึง 2 เท่า
+          </div>
+          <div style="width: 100%">
+            <div class="button-but">ซื้อเลย</div>
+          </div>
+        </div>
+        <div class="card-other">
+          <div>
+            <span class="font-card-top-other">฿29</span
+            ><span class="font-right-other">/month</span>
+            <div>เฉลี่ย 0.97 ต่อวัน</div>
+          </div>
+
+          <div class="font-right-other" style="font-size: 24px">Pro</div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              สิทธิพิเศษจากการบริการจาก
+              <span class="font-right-other">Brand</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />ได้รับส่วนลดจากร้านค้าเพิ่ม สูงสุด 5 - 50 %
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />มีระบบแจ้งเตือน SMS เมื่อ ทำการถอนเงินสำเร็จ
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              ได้รับสัญลักษณ์ว่าเป็นร้านค้า
+              <span class="font-right-other">"Brand"</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />เพิ่มรูปภาพสินค้าได้มากกว่าเดิม ถึง 2 เท่า
+          </div>
+          <div style="width: 100%">
+            <div class="button-but">ซื้อเลย</div>
+          </div>
+        </div>
+        <div class="card-other">
+          <div>
+            <span class="font-card-top-other">฿29</span
+            ><span class="font-right-other">/month</span>
+            <div>เฉลี่ย 0.97 ต่อวัน</div>
+          </div>
+
+          <div class="font-right-other" style="font-size: 24px">Enterprise</div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              สิทธิพิเศษจากการบริการจาก
+              <span class="font-right-other">Brand</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />ได้รับส่วนลดจากร้านค้าเพิ่ม สูงสุด 5 - 50 %
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />มีระบบแจ้งเตือน SMS เมื่อ ทำการถอนเงินสำเร็จ
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />
+            <div>
+              ได้รับสัญลักษณ์ว่าเป็นร้านค้า
+              <span class="font-right-other">"Brand"</span>
+            </div>
+          </div>
+          <div class="font-card">
+            <img
+              class=""
+              style="margin: 0px 10px 0px 0px"
+              src="~/assets/image/tred.png"
+            />เพิ่มรูปภาพสินค้าได้มากกว่าเดิม ถึง 2 เท่า
+          </div>
+          <div style="width: 100%">
+            <div class="button-but">ซื้อเลย</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div
+      class="form-create"
+      style="margin-top: 20px; gap: var(--Spacing-space-16, 12px)"
+    >
+      <div class="font-form-pak">
+        Become <span style="color: #bababa">“BRAND”</span>
+      </div>
+      <div>
+        <img class="img-ganaral" src="~/assets/image/General.png" />
+      </div>
+      <div class="font-form-pak">General</div>
+      <div class="content-pakg">
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สิทธิพิเศษในการได้รับ การบริการ จาก Brand
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          โบนัส การอัพเลเวล Ranking และ โบนัส Reward จากการทำกิจกรรม ต่างๆ 110 %
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          การซื้อ Gamqio plus 30 วันขึ้นไป จะได้รับสิทธิ์ Ranking Protection ตาม
+          package นั้นๆ
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          มีระบบแจ้งเตือน SMS เมื่อทำการ ถอนเงินสำเร็จ
+        </div>
+      </div>
+
+      <div style="width: 100%; text-align: center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="824"
+          height="2"
+          viewBox="0 0 824 2"
+          fill="none"
+        >
+          <path
+            d="M0.5 1H823.5"
+            stroke="#5C25F2"
+            stroke-width="2"
+            stroke-dasharray="10 10"
+          />
+        </svg>
+      </div>
+      <div>
+        <img class="img-ganaral" src="~/assets/image/General.png" />
+      </div>
+      <div class="font-form-pak">Marketplace</div>
+      <div class="font-form-buy">Buyer</div>
+      <div class="content-pakg">
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สะสมคูปองส่วนลดมากขึ้น (เฉพาะ คูปองสำหรับสมาชิก gamiqo+ เท่านั้น)
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          Refund สินค้าได้มากกว่า 2 ครั้ง ต่อเดือน (ตามเงื่อนไขที่กำหนด)
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          ได้รับส่วนลดจากร้านค้า Gamiqo official เพิ่มสูงสุด 5 - 50 %
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          ได้รับโบนัสคะแนน Ranking และ Reward จากการซื้อสินค้า 110 %
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สิทธิพิเศษสำหรับซื้อสินค้า เฉพาะ สมาชิก Gamiqo+ เท่านั้น
+        </div>
+      </div>
+      <div class="font-form-buy">Seller</div>
+      <div class="content-pakg">
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สร้างคูปองส่วนลด สำหรับใช้บน ร้านค้าของคุณเองได้
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          เพิ่มรูปภาพสินค้าได้มากกว่าเดิม ถึง 2 เท่า
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สามารถใส่ลิงก์วิดีโอได้
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          ได้รับสัญลักษณ์ว่าเป็นร้านค้า "Gamiqo+"
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สินค้าของร้านทุกชิ้น จะขึ้นแสดงใน หน้า "Gamiqo+ Marketplace"
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สินค้าของร้านบางชิ้นจะขึ้นแสดง ในส่วน"Recommended"
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สามารถวางขายไอเทมในเกมที่มี ราคาสูงกว่า 1,000 บาทได้
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          สามารถลงทะเบียนสินค้าประเภท “การบริการ(service)” ได้
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          มีระบบแจ้งเตือนทาง SMS เมื่อมี รายการ Booking สินค้า
+        </div>
+        <div class="wid-content">
+          <div>
+            <img class="button-dot" src="~/assets/image/button.png" />
+          </div>
+          มีระบบแจ้งเตือนทาง SMS เมื่อได้ รับเงินจากการขายสินค้า
+        </div>
+      </div>
+      <div style="width: 100%; text-align: center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="824"
+          height="2"
+          viewBox="0 0 824 2"
+          fill="none"
+        >
+          <path
+            d="M0.5 1H823.5"
+            stroke="#5C25F2"
+            stroke-width="2"
+            stroke-dasharray="10 10"
+          />
+        </svg>
+      </div>
+      <div>
+        <img class="img-ganaral" src="~/assets/image/General.png" />
+      </div>
+      <div class="font-form-pak">Esports</div>
+      <div class="font-form-buy">Promoter</div>
+      <div class="wid-content">
+        <div>
+          <img class="button-dot" src="~/assets/image/button.png" />
+        </div>
+        Coming Soon
+      </div>
+      <div class="font-form-buy">Players</div>
+      <div class="wid-content">
+        <div>
+          <img class="button-dot" src="~/assets/image/button.png" />
+        </div>
+        Coming Soon
+      </div>
+      <div style="width: 100%; text-align: center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="824"
+          height="2"
+          viewBox="0 0 824 2"
+          fill="none"
+        >
+          <path
+            d="M0.5 1H823.5"
+            stroke="#5C25F2"
+            stroke-width="2"
+            stroke-dasharray="10 10"
+          />
+        </svg>
+      </div>
+      <div>
+        <img class="img-ganaral" src="~/assets/image/General.png" />
+      </div>
+      <div class="font-form-pak">Job Board</div>
+      <div class="font-form-buy">Applicant</div>
+      <div class="wid-content">
+        <div>
+          <img class="button-dot" src="~/assets/image/button.png" />
+        </div>
+        Coming Soon
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.card-into {
+  border-radius: var(--Border-radius-8, 8px);
+  border: 2px dashed var(--Color-Secondary-500, #ffeb3b);
+  background: var(
+    --Linear,
+    linear-gradient(88deg, #6127ff 0.47%, #6f3aff 100%)
+  );
+  display: flex;
+  width: 253px;
+  padding: var(--Spacing-space-16, 16px) var(--Spacing-space-24, 24px);
+  flex-direction: column;
+  gap: var(--Spacing-space-16, 12px);
+  align-items: flex-start;
+  margin-right: 16px;
+  height: 470px;
+}
+.card-other {
+  display: flex;
+  width: 253px;
+  padding: var(--Spacing-space-16, 16px) var(--Spacing-space-24, 24px);
+  flex-direction: column;
+  align-items: flex-start;
+  gap: var(--Spacing-space-16, 12px);
+  border-radius: var(--Border-radius-8, 8px);
+  background: var(--color-black-white-900, #fff);
+  color: #000;
+  margin-right: 16px;
+}
+.poppular {
+  padding: var(--Spacing-space-4, 4px) var(--Spacing-space-8, 8px);
+  border-radius: var(--Border-radius-Full, 9999px);
+  background: var(--Color-Primary-400, #8153ff);
+  color: #ffeb3b;
+  left: 100px;
+  font-size: 14px;
+  position: relative;
+}
+.dis-flex-end {
+  display: flex;
+  justify-content: center;
+  flex-wrap: nowrap;
+  height: 500px;
+  align-items: flex-end;
+}
+.font-card {
+  display: flex;
+  flex-direction: row;
+  font-size: 14px;
+}
+
+.font-card-top {
+  color: var(--Color-Secondary-500, #ffeb3b);
+  font-size: 48px;
+}
+.font-card-top-other {
+  color: var(--Color-Secondary-500, #5c25f2);
+  font-size: 48px;
+}
+.font-right-other {
+  color: #5c25f2;
+}
+.button-but {
+  display: flex;
+  height: 36px;
+  padding: var(--Spacing-space-8, 8px) var(--Spacing-space-12, 12px);
+  justify-content: center;
+  align-items: center;
+  gap: var(--Spacing-space-8, 8px);
+  align-self: stretch;
+  border-radius: var(--Border-radius-8, 8px);
+  border: 1px solid var(--Color-Primary-Pueple-500, #5c25f2);
+  background: var(--Color-Secondary-500, #ffeb3b);
+  color: var(--Color-Primary-Pueple-500, #5c25f2);
+  width: 100%;
+  cursor: pointer;
+}
+.font-form-pak {
+  font-size: 48px;
+  color: #5c25f2;
+}
+.font-form-buy {
+  color: #bababa;
+  font-size: 32px;
+}
+.img-ganaral {
+  margin: 10px 10px 10px 0px;
+  display: flex;
+  padding: var(--Spacing-space-16, 16px);
+  align-items: flex-start;
+  border-radius: var(--Border-radius-20, 20px);
+  background: rgba(92, 37, 242, 0.2);
+}
+.content-pakg {
+  display: flex;
+  padding: 0px;
+  padding: var(--Spacing-space-0, 0px);
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 16px;
+  gap: var(--Spacing-space-16, 16px);
+  width: 100%;
+  flex-wrap: wrap;
+}
+.wid-content {
+  width: 30%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+}
+.button-dot {
+  margin-right: 10px;
+  margin-bottom: 5px;
+}
+</style>
---
+++ pages/MarketPackage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="box-cotent">
+      <div class="W-default ">
+        <MarketPackage />
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+
