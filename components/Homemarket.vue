d5ecea25aa1d62f03fdd9d341f42c305e6864cca
pcnadev <boombs5665@gmail.com>
2024-07-10 16:16:08 +0700

    Projectgamitoe

--- components/Homemarket.vue
+++ components/Homemarket.vue
@@ -85,7 +85,7 @@ export default {
     };
   },
   mounted() {
-    this.fetchData();
+    // this.fetchData();
   },
   methods: {
     async fetchData() {
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    target: 'server', // หรือ 'static' ตามที่คุณต้องการ
-    ssr: true, // เปิดใช้งาน Server-Side Rendering
     title: 'gamiqo',
     htmlAttrs: {
       lang: 'en'
@@ -20,7 +18,6 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'bootstrap/dist/css/bootstrap.min.css',
     '~/assets/scss/main.scss', '~/assets/scss/navbar.scss', '~/assets/scss/market.scss', '~/assets/scss/Shop.scss',
     'slick-carousel/slick/slick.css',
     'slick-carousel/slick/slick-theme.css'
@@ -49,31 +46,6 @@ export default {
   ],
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend(config, { isDev, isClient }) {
-      if (!isDev && isClient) {
-        config.output.hashFunction = 'sha256';
-      }
-    },
-    loaders: {
-      scss: { implementation: require('sass') }
-    }
   },
-  routeRules: {
-    // Homepage pre-rendered at build time
-    '/': { prerender: true },
-    // Products page generated on demand, revalidates in background, cached until API response changes
-    '/products': { swr: true },
-    // Product page generated on demand, revalidates in background, cached for 1 hour (3600 seconds)
-    '/products/**': { swr: 3600 },
-    // Blog posts page generated on demand, revalidates in background, cached on CDN for 1 hour (3600 seconds)
-    '/blog': { isr: 3600 },
-    // Blog post page generated on demand once until next deployment, cached on CDN
-    '/blog/**': { isr: true },
-    // Admin dashboard renders only on client-side
-    '/admin/**': { ssr: false },
-    // Add cors headers on API routes
-    '/api/**': { cors: true },
-    // Redirects legacy urls
-    '/old-page': { redirect: '/new-page' }
-  }
+
 }
